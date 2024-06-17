1add0a34be4bdc4c81f7fa74adc85645b7d31240
Jack Li <jack.li@digitalcni.com>
2024-06-17 10:59:29 -0400

    refactor - simplified syntax

--- bird-watcher/BirdWatcher.cs
+++ bird-watcher/BirdWatcher.cs
@@ -10,23 +10,15 @@ class BirdCount
         this.birdsPerDay = birdsPerDay;
     }
 
-    public static int[] LastWeek() => new int[] { 0, 2, 5, 3, 7, 8, 4 };
+    public static int[] LastWeek() => new[] { 0, 2, 5, 3, 7, 8, 4 };
     
     public int Today() => this.birdsPerDay.LastOrDefault();
 
     public void IncrementTodaysCount() => this.birdsPerDay[^1]++;
 
-    public bool HasDayWithoutBirds() => Array.Exists(this.birdsPerDay, e => e == 0);
+    public bool HasDayWithoutBirds() => birdsPerDay.Contains(0);
 
-    public int CountForFirstDays(int numberOfDays)
-    {
-        int total = 0;
-        for (int i = 0; i < numberOfDays; i++)
-        {
-            total += this.birdsPerDay[i];
-        }
-        return total;
-    }
+    public int CountForFirstDays(int numberOfDays) => birdsPerDay.Take(numberOfDays).Sum();
 
     public int BusyDays() => this.birdsPerDay.Count(c => c >= 5);
 }
