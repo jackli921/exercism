d476deb1f7ba1c43df72781298d3fd03661f3d73
Jack Li <jack.li@digitalcni.com>
2024-06-17 11:35:08 -0400

    initial commit

---
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,40 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (knightIsAwake || archerIsAwake || prisonerIsAwake) 
+            return true;
+        return false;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return !archerIsAwake && prisonerIsAwake ? true : false;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent)
+        {
+            if (!archerIsAwake)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (!knightIsAwake && !archerIsAwake && prisonerIsAwake)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
