4cb603fd33c9152833d9ce5e3b572935658ea17f
Jack Li <jack.li@digitalcni.com>
2024-06-21 15:56:33 -0400

    initial commit

--- interest-is-interesting/InterestIsInteresting.cs
+++ interest-is-interesting/InterestIsInteresting.cs
@@ -2,23 +2,26 @@ using System;
 
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
+    public static float InterestRate(decimal balance) => balance switch
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
-    }
+        < 0 => 3.213f,
+        < 1000 => 0.5f,
+        >= 1000 and < 5000 => 1.621f,
+        _ => 2.475f
+    };
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
+    public static decimal Interest(decimal balance) => balance * (decimal)InterestRate(balance) / 100;
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int year = 0;
+        while (targetBalance > balance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            year++;
+        }
+        return year;
     }
 }
