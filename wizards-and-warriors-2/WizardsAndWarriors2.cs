458c97f5bfe98cca55e0e2bd28ddcba1d3db1a9a
Jack Li <jack.li@digitalcni.com>
2024-06-23 11:53:17 -0400

    revised solution
    - Reuse other overloads to simplify implementation

---
+++ wizards-and-warriors-2/WizardsAndWarriors2.cs
@@ -0,0 +1,37 @@
+using System;
+
+static class GameMaster
+{
+    public static string Describe(Character character) => $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
+
+    public static string Describe(Destination destination) =>
+        $"You've arrived at {destination.Name}, which has {destination.Inhabitants} inhabitants.";
+
+    public static string Describe(TravelMethod travelMethod) =>
+        $"You're traveling to your destination {(travelMethod == TravelMethod.Horseback ? "on horseback." : "by walking.")}";
+
+    public static string Describe(Character character, Destination destination, TravelMethod travelMethod) =>
+        $"{Describe(character)} {Describe(travelMethod)} {Describe(destination)}";
+    
+    public static string Describe(Character character, Destination destination) =>
+        Describe(character, destination, TravelMethod.Walking);
+}
+
+class Character
+{
+    public string Class { get; set; }
+    public int Level { get; set; }
+    public int HitPoints { get; set; }
+}
+
+class Destination
+{
+    public string Name { get; set; }
+    public int Inhabitants { get; set; }
+}
+
+enum TravelMethod
+{
+    Walking,
+    Horseback
+}
