5d5951a1ca66ad244d058063e842e8ebbb21591b
Jack Li <jack.li@digitalcni.com>
2024-06-29 17:06:22 -0400

    beer-song
    - using LINQ to generate looping range

--- beer-song/BeerSong.cs
+++ beer-song/BeerSong.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Linq;
 using System.Text;
 
 public static class BeerSong
 {
-    public static string Recite(int startBottles, int takeDown)
+    public static string Recite(int startBottles, int Takedown) => string.Join("\n\n", Enumerable.Range((startBottles - (Takedown - 1)), Takedown).Reverse().Select(x => Verse(x)));
+    public static string Verse(int bottles) => bottles switch
+    {
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.",
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.",
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.",
+        _ => $"{bottles} bottles of beer on the wall, {bottles} bottles of beer.\nTake one down and pass it around, {bottles - 1} bottles of beer on the wall."
+    };
+    /*public static string Recite(int startBottles, int takeDown)
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < takeDown ; i++)
+        {
+            int currentBottle = startBottles - i;
+            switch (currentBottle)
+            {
+                case (0) :
+                    result.Append("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+                    break;
+                case (1):
+                    result.Append($"1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.");
+                    break;
+                case (2):
+                    result.Append($"2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.");
+                    break;
+                default:
+                    result.Append($"{currentBottle} bottles of beer on the wall, {currentBottle} bottles of beer.\nTake one down and pass it around, {currentBottle - 1} bottles of beer on the wall.");
+                    break;
+            }
+
+            if (i < takeDown - 1)
+            {
+                result.Append("\n\n");
+            }
+        }
+        return result.ToString().Replace("\r\n", "\n");
+    }*/
+
+    /*public static string Recite(int startBottles, int takeDown)
     {
         StringBuilder result = new StringBuilder();
 
@@ -11,9 +51,8 @@ public static class BeerSong
         {
             switch (i)
             {
-                 case (0) :
-                     result.Append("No more bottles of beer on the wall, no more bottles of beer.\n" +
-                                   "Go to the store and buy some more, 99 bottles of beer on the wall.");
+                 case (0) :result.Append("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                                          "Go to the store and buy some more, 99 bottles of beer on the wall.");
                      break;
                  case (1):
                      result.Append($"1 bottle of beer on the wall, 1 bottle of beer.\n" + 
@@ -36,45 +75,7 @@ public static class BeerSong
             }
         }
         return result.ToString();
-    }
+    }*/
 }
 
-// public static class BeerSong
-// {
-//     public static string Recite(int startBottles, int takeDown)
-//     {
-//         StringBuilder result = new StringBuilder();
-//
-//         for (int i = 0; i < takeDown ; i++)
-//         {
-//             int currentBottle = startBottles - i;
-//             switch (currentBottle)
-//             {
-//                 case (0) :
-//                     result.Append("No more bottles of beer on the wall, no more bottles of beer.\n" +
-//                                   "Go to the store and buy some more, 99 bottles of beer on the wall.");
-//                     break;
-//                 case (1):
-//                     result.Append($"1 bottle of beer on the wall, 1 bottle of beer.\n" + 
-//                                   "Take it down and pass it around, no more bottles of beer on the wall.");
-//                     break;
-//
-//                 case (2):
-//                     result.Append($"2 bottles of beer on the wall, 2 bottles of beer.\n" +
-//                                   "Take one down and pass it around, 1 bottle of beer on the wall.");
-//                     break;
-//
-//                 default:
-//                     result.Append($"{currentBottle} bottles of beer on the wall, {currentBottle} bottles of beer.\n" + 
-//                                   $"Take one down and pass it around, {currentBottle - 1} bottles of beer on the wall.");
-//                     break;
-//             }
-//
-//             if (i < takeDown - 1)
-//             {
-//                 result.Append("\n\n");
-//             }
-//         }
-//         return result.ToString().Replace("\r\n", "\n");
-//     }
-// }
+
--- beer-song/obj/Debug/net8.0/BeerSong.AssemblyInfo.cs
+++ beer-song/obj/Debug/net8.0/BeerSong.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeerSong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543fed3beb93b0f478e44a99792e6458c94938cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab5f6619f75dd7ad5033bbf7cf02af1d288b5e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeerSong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeerSong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
