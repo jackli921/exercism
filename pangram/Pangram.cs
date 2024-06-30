59303137bba67cfe02144fc1fcd2260711bbbd1c
Jack Li <jack.li@digitalcni.com>
2024-06-30 19:12:04 -0400

    pangram
    - use .All() and .Contains()

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -4,5 +4,10 @@ using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input) => input.ToLower().Distinct().Where(char.IsLetter).Count() == 26;
+    public static bool IsPangram(string input)
+    {
+        // return input.ToLower().Distinct().Where(char.IsLetter).Count() == 26;
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        return alphabet.All(c => input.ToLower().Contains(c));
+    }
 }
