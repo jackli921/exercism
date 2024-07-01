924ae5ee2c800c7a6ceb882d996f95f772fa273c
Jack Li <jack.li@digitalcni.com>
2024-07-01 07:25:32 -0400

    reverse-string
    - Enumerate IEnumerable<char> into array then into a string

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return new string(input.Reverse().ToArray());
     }
 }
