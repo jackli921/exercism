472cf65cd4b0eaa72425c8700cf54ec843291f4c
Jack Li <jack.li@digitalcni.com>
2024-07-02 07:29:34 -0400

    - use ternary or chained boolean

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -7,7 +7,7 @@ public static class Leap
 
         if (year % 4 == 0 && year % 100 != 0)
             return true;
-        if (year % 400 == 0)
+        if (year % 100 == 0 && year % 400 == 0)
             return true;
         return false;
         
