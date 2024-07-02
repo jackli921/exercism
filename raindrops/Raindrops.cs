ed6ddedeecbbe68db95d82ee6ec15be750cdab2e
Jack Li <jack.li@digitalcni.com>
2024-07-02 16:33:54 -0400

    raindrops
    use stringBuilder & Append

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+
+        var result = new StringBuilder();
+        if (number % 3 == 0) result.Append("Pling");
+        if (number % 5 == 0) result.Append("Plang");
+        if (number % 7 == 0) result.Append("Plong");
+        return result.Length != 0 ? result.ToString() : number.ToString();
+    }
+}
