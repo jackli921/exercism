5e1df68c296a1ac441780428712e3ebe66d9dea3
Jack Li <jack.li@digitalcni.com>
2024-07-03 07:11:27 -0400

    perfect-numbers
    - use Enumerable.Range, Where(), Sum(), CompareTo()

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -4,14 +4,16 @@ using System.Linq;
 
 public enum Classification
 {
-    Perfect,
-    Abundant,
-    Deficient
+    Perfect = 0,
+    Abundant = 1,
+    Deficient = -1
 }
 
 public static class PerfectNumbers
 {
-    public static Classification Classify(int number)
+    public static Classification Classify(int number) =>
+        (Classification)Enumerable.Range(1, number - 1).Where(i => number % i == 0).Sum().CompareTo(number);
+    /*public static Classification Classify(int number)
     {
         // use a for loop, and count from the number itself down to 1
         // if the number is divisible by the current iteration number of i with a remainder of 0, then add that number to the number list
@@ -36,5 +38,5 @@ public static class PerfectNumbers
         if (sum == number) return Classification.Perfect;
         if (sum > number) return Classification.Abundant;
         return Classification.Deficient;
-    }
+    }*/
 }
--- perfect-numbers/obj/Debug/net8.0/PerfectNumbers.AssemblyInfo.cs
+++ perfect-numbers/obj/Debug/net8.0/PerfectNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerfectNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6ddedeecbbe68db95d82ee6ec15be750cdab2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ca008258b80b22dc76611ac1f9c2e48541e33f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerfectNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerfectNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
