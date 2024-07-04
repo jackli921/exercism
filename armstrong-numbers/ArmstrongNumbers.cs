698f169cc5b524642ea643467d0e20e928fcae71
Jack Li <jack.li@digitalcni.com>
2024-07-04 15:13:27 -0400

    armstrong-numbers
    - using Math.Pow() to raise number to the specified power
    
    - use char.GetNumericValue() to convert char to int

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
-    public static bool IsArmstrongNumber(int number)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsArmstrongNumber(int number) =>
+        number.ToString()
+            .Select(x => Math.Pow(char.GetNumericValue(x), number.ToString().Length))
+            .Sum() == number;
 }
